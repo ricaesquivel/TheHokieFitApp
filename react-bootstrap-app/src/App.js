8203703f1f9f04b540ca3ff8ecfc860d5fe5ca6c
rbgary <rbgary@vt.edu>
2024-02-03 16:58:56 -0500

    Added base files

---
+++ react-bootstrap-app/src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          This is so much more difficult than it needs to be :(
+        </p>
+        <MyButton />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+function MyButton() {
+  return ( 
+    <button onClick={onClick}>
+      I am a button!
+    </button>
+  );
+}
+
+function onClick() {
+  alert("This works!");
+}
